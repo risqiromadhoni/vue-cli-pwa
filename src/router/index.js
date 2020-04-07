e0b725943952f8e09390f0bdac5546f2adcdb96a
kali <risqir57@gmail.com>
2020-04-07 07:23:43 -0400

    dinamic router mode

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,13 @@ import { routes } from "./routes";
 
 Vue.use(VueRouter);
 
+let router_mode = "hash";
+if (process.env.NODE_ENV === "production") {
+  router_mode = "history";
+}
+
 const router = new VueRouter({
+  mode: router_mode,
   routes
 });
 
