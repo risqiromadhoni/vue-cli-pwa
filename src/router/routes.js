d6a0407d82426682baee67d862ba9d5dd20ae53e
kali <risqir57@gmail.com>
2020-04-07 06:47:24 -0400

    home fixed

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ export const routes = [
   {
     path: "/",
     name: "Home",
-    component: import(/* webpackChunkName: "home" */ "../views/Home.vue")
+    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
   },
   {
     path: "/about",
