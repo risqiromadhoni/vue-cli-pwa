f563eea5ab645f08220c2542ef96e94525fa4c92
kali <risqir57@gmail.com>
2020-04-17 05:52:38 -0400

    fixed order wizard

---
+++ src/components/frontend/utils/NasiBoksPage/OrderWizard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="section-wrapper">
+    <ul class="tab-bar d-none d-md-flex">
+      <li
+        v-for="step in dataForm.step"
+        :key="step.component"
+        :class="{ tablinks: true, active: step.isActive }"
+        @click="actionStep(step.component)"
+      >
+        <img v-lazy="step.img" alt="food-product" />
+        <span>{{ step.name | capitalize }}</span>
+      </li>
+    </ul>
+    <keep-alive>
+      <component :is="formPage"></component>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderFormWizard",
+  data() {
+    return {
+      formPage: "WizardFormOne"
+    };
+  },
+  props: {
+    dataForm: Object
+  },
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    WizardFormOne: async () => await import(`./WizardFormOne`),
+    // eslint-disable-next-line vue/no-unused-components
+    WizardFormTwo: async () => await import(`./WizardFormTwo`),
+    // eslint-disable-next-line vue/no-unused-components
+    WizardFormThree: async () => await import(`./WizardFormThree`)
+  },
+  mounted() {
+    console.log(this.dataForm);
+  },
+  methods: {
+    actionStep: function(param) {
+      this.formPage = param;
+      this.dataForm.step.map(m => {
+        return m.component == this.formPage
+          ? (m.isActive = true)
+          : (m.isActive = false);
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/style/frontend/scss/custom";
+</style>
---
+++ src/components/frontend/utils/NasiBoksPage/WizardFormTwo.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-row>
+    <b-col sm>
+      <div>Two</div>
+    </b-col>
+    <b-col sm>
+      <div class="contact-form">
+        <form action="https://www.foxcoders.com/">
+          <input type="text" name="name" placeholder="Full Name*" />
+          <input type="email" name="email" placeholder="Your Eamil*" />
+          <input type="text" name="number" placeholder="Phone Number" />
+          <div class="res-tab time-zone">
+            <select>
+              <option value="1">Enter Time</option>
+              <option value="2">10:00 AM</option>
+              <option value="3">11:00 AM</option>
+              <option value="4">12:00 PM</option>
+              <option value="5">01:00 PM</option>
+              <option value="6">02:00 PM</option>
+              <option value="7">03:00 PM</option>
+              <option value="8">04:00 PM</option>
+              <option value="9">05:00 PM</option>
+            </select>
+          </div>
+          <div class="res-tab table-zone">
+            <select>
+              <option value="1">Room Type</option>
+              <option value="2">Normal</option>
+              <option value="3">VIP</option>
+              <option value="4">VVIP</option>
+            </select>
+          </div>
+          <input type="date" name="time" data-provide="datepicker" />
+          <textarea name="message" rows="6" placeholder="Message"></textarea>
+          <button type="submit" class="food-btn style-2">
+            <span>book a table</span>
+          </button>
+        </form>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
